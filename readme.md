72a01899f8471371712b601123912a681b7b3929
Yash Mishra <yashrkmfedu6927@gmail.com>
2023-04-29 16:47:30 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# QR Generator 
+
+<img src="./screenshot/Screenshot (265).png">
+<img src="./screenshot/Screenshot (266).png">
+
+## Introduction
+
+This is a small and fun project that I created using HTML, CSS, and JavaScript. It's a QR code generator that allows anyone to create QR codes for any link or text, and even download them!
+
+## How to Use
+
+To use the QR generator, simply enter the link or text you want to generate a QR code for, and click on the "Generate QR" button. You can then download the QR code by clicking on the "Download QR" button.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- QRCode.js (GitHub library)
+
+## Installation
+
+To install the project, simply clone this repository to your local machine and open the index.html file in your web browser.
+
+git clone https://github.com/your-yashmishra0101/qr-generator.git
+
+
+## Conclusion
+
+This project was a lot of fun to make and a great way to practice my web development skills. I hope you find it useful and enjoyable to use. If you have any feedback or suggestions, feel free to leave a comment or create a pull request.
---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("checking")
+const input_field=document.querySelector('[data-input-field]');
+const select_sizes=document.querySelector('[data-select-sizes]');
+const qr_image=document.querySelector('[data-qr-image]');
+const generate_button=document.querySelector('[data-generate-button]');
+const download_button=document.querySelector('[data-download-button]')
+const qr_image_container=document.querySelector('.qr-image-container')
+
+
+let size=select_sizes;
+size="100"
+select_sizes.addEventListener('change',(e)=>{
+    size=e.target.value;
+})
+
+const qr_code_info=()=>{
+    new QRCode(qr_image_container, {
+        text:input_field.value,
+        width: size,
+        height: size,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+}
+
+generate_button.addEventListener('click',()=>{
+    qr_image_container.innerText="";
+    qr_code_info();
+})
+
+download_button.addEventListener('click',()=>{
+    let qr_body_img=document.querySelector('.qr-image-container img')
+    if( qr_body_img!== null){
+        let qr_body_img=document.querySelector('.qr-image-container img')
+        let img_src = qr_body_img.getAttribute('src');
+        download_button.setAttribute('href', img_src);
+    }
+    else{
+        alert("Empty QR")
+    }
+})
+     
